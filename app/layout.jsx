56762b6f51b6dc4149d1cc6899a55a49ccb1073d
Onkar Patel <onkarpatel10@gmail.com>
2025-07-02 11:53:11 +0530

    refactor: restructure and style Projects section as a reusable component

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,8 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        {/* <Header /> */}
+      <body className={'antialiased'}>
+        <Header />
         {children}
       </body>
     </html>
