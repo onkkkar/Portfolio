0f9645994ca1ca0f7fe809785aea2988e7270385
Onkar Patel <onkarpatel10@gmail.com>
2025-07-28 12:20:00 +0530

    feat: Updated Navbar Socials

--- components/Header/navigation/Footer/page.jsx
+++ components/Header/navigation/Footer/page.jsx
@@ -1,4 +1,6 @@
 import styles from './style.module.scss';
+import Magnetic from '../../../../common/Magnetic/page';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -7,9 +9,33 @@ const Footer = () => {
         <p>Socials</p>
       </div>
       <div className={styles.socials}>
-        <a>Twitter</a>
-        <a>GitHub</a>
-        <a>LinkedIn</a>
+        <Magnetic>
+          <Link
+            href='https://x.com/wannabe_sde'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Twitter
+          </Link>
+        </Magnetic>
+        <Magnetic>
+          <Link
+            href='https://github.com/onkar-03'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            GitHub
+          </Link>
+        </Magnetic>
+        <Magnetic>
+          <Link
+            href='https://www.linkedin.com/in/onkarpatel03/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            LinkedIn
+          </Link>
+        </Magnetic>
       </div>
     </div>
   );
